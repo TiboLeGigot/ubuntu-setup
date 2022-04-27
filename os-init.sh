b3379449ce6a36834d9587ccb81c4e248fecdb97
Thibault <t.jeannequin@gmail.com>
2022-04-27 17:35:32 +0200

    Finition de la version entièrement automatique

--- os-init.sh
+++ os-init.sh
@@ -44,8 +44,8 @@ git config --global user.name "$name"
 git config --global -l | grep user # Output la config git établie par le script
 
 # VS Code
+code="VS Code"
 if [ "$automated" = false ]; then
-    code="VS Code"
     read -p "Installer $code (y/n) ? " answer
     answer=${answer:-y}
     case ${answer:0:1} in y | Y)
@@ -59,8 +59,8 @@ else
 fi
 
 # Google Chrome
+chrome="Google Chrome"
 if [ "$automated" = false ]; then
-    chrome="Google Chrome"
     read -p "Installer $chrome (y/n) ? " answer
     case ${answer:0:1} in y | Y)
         echo Installation de $chrome...
@@ -78,21 +78,47 @@ fi
 
 # Brave browser
 brave="Brave Browser"
-read -p "Installer $brave (y/n) ? " answer
-case ${answer:0:1} in y | Y)
+if [ "$automated" = false ]; then
+    read -p "Installer $brave (y/n) ? " answer
+    case ${answer:0:1} in y | Y)
+        echo Installation de $brave...
+        sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+        sudo apt update
+        sudo apt install brave-browser
+        ;;
+    esac
+else
     echo Installation de $brave...
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
     sudo apt update
     sudo apt install brave-browser
-    ;;
-esac
+fi
 
 # Docker
 docker="docker"
-read -p "Installer $docker (y/n) ? " answer
-case ${answer:0:1} in y | Y)
-    echo Installation de $docker...
+if [ "$automated" = false ]; then
+    read -p "Installer $docker (y/n) ? " answer
+    case ${answer:0:1} in y | Y)
+        echo Installation de $docker...
+        sudo apt install \
+            ca-certificates \
+            gnupg \
+            lsb-release
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+        sudo apt-get update
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+        sudo groupadd docker
+        sudo usermod -aG docker $USER
+        newgrp docker
+        ;;
+    esac
+else
+   echo Installation de $docker...
     sudo apt install \
         ca-certificates \
         gnupg \
@@ -106,7 +132,6 @@ case ${answer:0:1} in y | Y)
     sudo groupadd docker
     sudo usermod -aG docker $USER
     newgrp docker
-    ;;
-esac
+fi
 
 echo Finite Incantatem !
